79e15a9c186184ed211a41d6ee00ff823f8ea71d
claudiavirlanuta <mirela.virlanuta@gmail.com>
2014-08-08 16:52:57 -0400

    Tested most functions and fixed bugs.

--- XMLgen.py
+++ XMLgen.py
@@ -21,9 +21,11 @@ newpath2 = r'Z:\partners\%s\request_%s_sf%s' % (newfolder, currentDate, newfolde
 if not os.path.exists(newpath2):
 	os.makedirs(newpath2)
 
+xmlFile = open("Z:\partners\%s\\request_%s_sf%s\ZI_%s_PersonMatch.xml" % (newfolder, currentDate, newfolder2, newfolder), "w")	
+
+
 # creates xml file header, which points to the location of the schema.
 def xmlHeader():
-	xmlFile = open("Z:\partners\%s\request_%s_sf%s\ZI_%s_PersonMatch.xml", "w") % (newfolder, currentDate, newfolder2)
 	xmlFile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
 	xmlFile.write("<beans xmlns=\"http://www.springframework.org/schema/beans\"\n")
 	xmlFile.write("       xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n")
@@ -44,29 +46,30 @@ def xmlHeader():
 def jobName():
 	job_name = raw_input("What is the job name? no spaces: ")
 	xmlFile.write("    <bean id=\"jobName\" class=\"java.lang.String\">\n")
-	xmlFile.write("        <constructor-arg value=\"%s\"/> <!-- LISTTYPE_(Titles) NO Date, NO SPACES-->\n") % job_name
+	xmlFile.write("        <constructor-arg value=\"%s\"/> <!-- LISTTYPE_(Titles) NO Date, NO SPACES-->\n" % job_name) 
 	xmlFile.write("    </bean>\n")
 
 # creates clientName bean.	
 def clientName():
 	client_name = raw_input("What is the client name? no spaces: ")
 	xmlFile.write("    <bean id=\"clientName\" class=\"java.lang.String\">\n")
-	xmlFile.write("        <constructor-arg value=\"%s\"/> <!-- Customer NAME NO Date, NO SPACES -->\n") % client_name
+	xmlFile.write("        <constructor-arg value=\"%s\"/> <!-- Customer NAME NO Date, NO SPACES -->\n" % client_name) 
 	xmlFile.write("    </bean>\n")
 
 #creates outputFolder bean.	
 def outputFolder():
 	xmlFile.write("    <bean id=\"outputFolder\" class=\"java.lang.String\">\n")
-	xmlFile.write("        <constructor-arg value=\"%s\"/> <!--FOLDER TO PUT OUTPUT FILE the FILE will be named automatically with Job name Client name and date -->\n") % newpath2
+	xmlFile.write("        <constructor-arg value=\"%s\"/> <!--FOLDER TO PUT OUTPUT FILE the FILE will be named automatically with Job name Client name and date -->\n" % newpath2) 
 	xmlFile.write("    </bean>\n")
 
+#creates personMatcher bean.
 def personMatcher():
 	inputFile = raw_input("What is the name of the input CSV file (without extension)? ")
 	xmlFile.write("    <bean id=\"personMatcher\" class=\"com.zoominfo.component.dsConfig.PersonMatcherConfig\">")
 	xmlFile.write("        <property name=\"inputDataFiles\">")
 	xmlFile.write("            <list>")
 	xmlFile.write("                <bean id=\"personMatchDataFile\" class=\"com.zoominfo.component.dsConfig.InputDataFileConfig\">")
-	xmlFile.write("                    <property name=\"filePath\" value=\"c:\partners\%s\request_%s_sf%s\%s.csv\"/> <!-- path to input company file name -->") % (newfolder, currentDate, newfolder2, inputFile)
+	xmlFile.write("                    <property name=\"filePath\" value=\"c:\partners\%s\request_%s_sf%s\%s.csv\"/> <!-- path to input company file name -->" % (newfolder, currentDate, newfolder2, inputFile)) 
 	xmlFile.write("                    <property name=\"hasHeader\" value=\"true\"/>")
 	xmlFile.write("                </bean>")
 	xmlFile.write("            </list>")
@@ -74,7 +77,18 @@ def personMatcher():
 	xmlFile.write("        <property name=\"columnMappings\">")
 	xmlFile.write("            <map>")
 	coNameKey = raw_input("Please enter the index of the company name column from the input file: ")
+	xmlFile.write()
+	xmlFile.write()
+	xmlFile.write()
+	xmlFile.write()
+	xmlFile.write()
+	
+xmlHeader()
+jobName()
+clientName()
+outputFolder()
 
+"""
 while True:	
 	try: 
 		firstKey = int(coNameKey)
@@ -84,4 +98,4 @@ while True:
 	xmlFile.write("			<entry key=\"%d\" value=\"personresumecompanyname\" />") % firstKey
 	xmlFile.write("")
 	xmlFile.write("")
-	
+"""
